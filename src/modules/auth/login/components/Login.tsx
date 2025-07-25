c9644a5cac7e009bd9628fde4eb173bb69a27aa3
Ahmed Samy <as8609377@gmail.com>
2025-07-25 19:27:54 +0300

    .

--- src/modules/auth/login/components/Login.tsx
+++ src/modules/auth/login/components/Login.tsx
@@ -1,17 +1,8 @@
 "use client";
-
 import { FcGoogle } from "react-icons/fc";
 import { FaUser, FaLock, FaFacebook } from "react-icons/fa";
-import { useEffect, useState } from "react";
 import { FaSquareXTwitter } from "react-icons/fa6";
-
 export default function LoginPage() {
-  
-
-
-
-  
-
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
       <div className="bg-white rounded-[10px] shadow-lg p-10  flex flex-col md:flex-row w-full max-w-[1500px] min-h-[500px]">
@@ -20,32 +11,20 @@ export default function LoginPage() {
 
           <div className="relative mb-4">
             <FaUser className="absolute left-3 top-3 text-black" />
-            <input
-              type="text"
-              placeholder="Enter Username"
-              className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              
-            />
+            <input type="text" placeholder="Enter Username" className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
           </div>
 
           <div className="relative mb-4">
             <FaLock className="absolute left-3 top-3 text-black" />
-            <input
-              type="password"
-              placeholder="Enter Password"
-              className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              
-            />
+            <input type="password" placeholder="Enter Password" className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
           </div>
 
           <div className="flex items-center mb-4">
-            <input id="remember" type="checkbox"  className="mr-2" />
+            <input id="remember" type="checkbox" className="mr-2" />
             <label htmlFor="remember">Remember Me</label>
           </div>
 
-          <button  className="bg-[#ff9090] hover:bg-[#f87272] text-white px-6 py-2 rounded-md mb-4 w-fit">
-            Login
-          </button>
+          <button className="bg-[#ff9090] hover:bg-[#f87272] text-white px-6 py-2 rounded-md mb-4 w-fit">Login</button>
 
           <div className="flex items-center mb-4">
             <span className="mr-2">Or, Login with</span>
